aa11a45108400f787c47663c4c225ba1131bb926
Arthuzin08 <Arthurlmsnts@gmail.com>
2026-05-12 16:15:59 -0300

    Adição da função de criar um atendente

--- Projeto-2-dados-main/Sistema.py
+++ Projeto-2-dados-main/Sistema.py
@@ -12,3 +12,14 @@ class Cliente:
             "telefone": self.telefone,
             "prioridade": self.prioridade
         }
+    
+class Atendente:
+    def __init__(self, id_atendente, nome):
+        self.id = id_atendente
+        self.nome = nome
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nome": self.nome
+        }
